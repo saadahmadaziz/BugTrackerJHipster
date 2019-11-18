cb5eaacddf430138a45de193ea92e5b4e7544067
Saad Ahmad <saadahmadaziz@gmail.com>
2019-11-18 22:16:08 +0500

    Initial version of BugTrackerJHipster generated by JHipster-6.5.0

---
+++ src/main/java/back/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package back.repository;
+
+import back.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/back/service/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package back.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
